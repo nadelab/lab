f4bd92a4cfe61e17739235596b89dd1befd0c4af
nade <i@nade.me>
2024-07-13 18:41:50 -0700

    cilium

--- init/dep.sh
+++ init/dep.sh
@@ -1,7 +1,7 @@
 # Add k8s repo
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" |
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" |
     tee /etc/apt/sources.list.d/kubernetes.list
 # Add CRI-O repo
 curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key |
@@ -10,7 +10,7 @@ echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.i
     tee /etc/apt/sources.list.d/cri-o.list
 
 # Install k8s
-apt-get install -y cri-o kubelet kubeadm kubectl
+apt-get install -y containerd kubelet kubeadm kubectl
 
 modprobe br_netfilter
 echo '1' > /proc/sys/net/ipv4/ip_forward
